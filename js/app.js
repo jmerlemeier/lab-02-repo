1ac8dda16c083e9db8396cc073c47a15172acb54
Susanna Lakey <susannalakey@gmail.com>
2019-08-15 07:44:26 -0700

    dropdowns for horns and keywords working

--- js/app.js
+++ js/app.js
@@ -19,30 +19,36 @@ const Image = function (image_url, title, description, keyword, numberofhorns) {
 Image.prototype.renderwithJquery = function () {
   const $myTemplate = $('#photo-template');
   const $myTemplateHtml = $myTemplate.html();
-
+  $myTemplate.hide();
   const $newSection = $('<section></section>')
   $newSection.html($myTemplateHtml);
 
   $newSection.find('h2').text(this.title);
   $newSection.find('img').attr('src', this.image_url);
   $newSection.find('p').text(this.description);
-  $newSection.find('img').attr('alt', this.keyword);
+  $newSection.find('img').attr('data-keyword', this.keyword);
+  $newSection.find('img').attr('data-horns', this.numberofhorns);
+
   $('main').append($newSection);
 }
 
 //AJAX
 const getAllImagesFromFile = () => {
   $.get('data/page-1.json').then(images => {
-    console.log('imgs from the then.', images);
-
     images.forEach(eachImage => {
-      new Image(eachImage.image_url, eachImage.title, eachImage.description, eachImage.keyword, eachImage.numberofhorns);
+      new Image(
+        eachImage.image_url,
+        eachImage.title,
+        eachImage.description,
+        eachImage.keyword,
+        eachImage.horns
+      );
     })
 
     allImages.forEach(image => {
       image.renderwithJquery();
     })
-    renderDropDown();
+
     //filterImages();
   })
 
@@ -51,26 +57,41 @@ const getAllImagesFromFile = () => {
 getAllImagesFromFile();
 //-------------------------------------
 
-function renderDropDown() {
-  const uniqueKeywords = [];
+function renderDropDown(attribute) {
+  const uniques = [];
   let dropdown = $('select');
-  allImages.forEach(value => {
+  allImages.forEach(image => {
     let flag = true;
-    uniqueKeywords.forEach(uniqueImage => {
-      if (uniqueImage === value.keyword) {
+    uniques.forEach(uniqueImage => {
+      if (uniqueImage === image[attribute]) {
         flag = false;
       }
+
     })
     if (flag) {
-      dropdown.append($('<option></option>').attr('value', value.keyword).text(value.keyword));
-      uniqueKeywords.push(value.keyword);
+      dropdown
+        .append($('<option></option>')
+          .attr('value', image[attribute])
+          .text(image[attribute]));
+      uniques.push(image[attribute]);
     }
   })
 }
 
 $('select').on('change', function () {
   let $selected = $(this).val();
-  console.log($selected);
   $('section').hide();
-  $(`img[alt = ${$selected}]`).parent().show();
+  $(`img[data-keyword = ${$selected}]`).parent().show();
+  $(`img[data-horns = ${$selected}]`).parent().show();
+});
+
+$('input[type=radio]').on('change', function () {
+  $('select').empty();
+  let $clicked = $(this).val();
+  console.log($(this).val())
+  if ($clicked === 'radio-one') {
+    renderDropDown('keyword');
+  } else {
+    renderDropDown('numberofhorns');
+  }
 });
